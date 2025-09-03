cead8977980c43632c94c70c3821b9d89b4f49e5
SalimIwiqja <salimemaataoui@gmail.com>
2025-09-03 13:04:27 +0100

    Update Unban.py

--- src/commands/dev/Unban.py
+++ src/commands/dev/Unban.py
@@ -1,6 +1,5 @@
 from libs import BaseCommand, MessageClass
 
-
 class Command(BaseCommand):
     def __init__(self, client, handler):
         super().__init__(
@@ -22,20 +21,12 @@ class Command(BaseCommand):
 
     def exec(self, M: MessageClass, contex):
         target = M.quoted_user or (M.mentioned[0] if M.mentioned else None)
-
         if not target:
-            return self.client.reply_message(
-                "⚠️ Mention or quote someone to unban.", M
-            )
+            return self.client.reply_message("⚠️ Mention or quote someone to unban.", M)
 
         user_data = self.client.db.get_user_by_number(target.number)
         if not user_data or not user_data.ban:
-            return self.client.reply_message(
-                f"ℹ️ *@{target.number.split('@')[0]}* is *not banned*.", M
-            )
+            return self.client.reply_message(f"ℹ️ *@{target.number.split('@')[0]}* is *not banned*.", M)
 
         self.client.db.update_user_ban(target.number, False, "No ban")
-
-        self.client.reply_message(
-            f"🔓 *@{target.number.split('@')[0]}* has been *unbanned*.", M
-        )
+        self.client.reply_message(f"🔓 *@{target.number.split('@')[0]}* has been *unbanned*.", M)
