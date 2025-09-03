1cfc089fb832338933433fc56480858524c76f3a
SalimIwiqja <salimemaataoui@gmail.com>
2025-09-03 13:03:46 +0100

    Update Ban.py

--- src/commands/dev/Ban.py
+++ src/commands/dev/Ban.py
@@ -1,6 +1,5 @@
 from libs import BaseCommand, MessageClass
 
-
 class Command(BaseCommand):
     def __init__(self, client, handler):
         super().__init__(
@@ -23,22 +22,20 @@ class Command(BaseCommand):
     def exec(self, M: MessageClass, contex):
         text = contex.text.strip()
         target = M.quoted_user or (M.mentioned[0] if M.mentioned else None)
+        if not target:
+            return self.client.reply_message("⚠️ Mention or quote someone to ban.", M)
+
+        # Split reason
         parts = text.split("|", 1)
         reason = parts[1].strip() if len(parts) > 1 else "No reason provided."
-        if not target:
-            return self.client.reply_message(
-                "⚠️ Mention or quote someone to ban.", M
-            )
 
         user_data = self.client.db.get_user_by_number(target.number)
         if user_data and user_data.ban:
             return self.client.reply_message(
-                f"⚠️ *@{target.number.split('@')[0]}* is already *banned*.\n📝 Reason: {user_data.reason}",
-                M,
+                f"⚠️ *@{target.number.split('@')[0]}* is already *banned*.\n📝 Reason: {user_data.reason}", M
             )
 
         self.client.db.update_user_ban(target.number, True, reason)
         self.client.reply_message(
-            f"🔒 *@{target.number.split('@')[0]}* has been *banned*.\n📝 *Reason:* {reason}",
-            M,
+            f"🔒 *@{target.number.split('@')[0]}* has been *banned*.\n📝 *Reason:* {reason}", M
         )
